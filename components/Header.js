1b85c8a69f28580b8df54113fc0300be05336195
frinzekt <frinzekt@gmail.com>
2020-03-02 11:52:47 +0800

    Typed Text

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Nav } from './Nav';
+import Typed from 'react-typed';
+
 export const Header = () => (
 	<section id='header' className='wrapper'>
-
 		<div id='logo'>
 			<h1>
-				<a href='index.html'>Frinze Erin Lapuz</a>
+				<a href='#'>Frinze Erin Lapuz</a>
 			</h1>
-			<p class='subtitle'>Software Engineering and Data Science Student</p>
-			<p>The Digital World is My Canvas</p>
+			<p className='subtitle'>
+				Software Engineering and Data Science Student
+				{/* <Typed strings={['Software Engineering and Data Science Student']} typeSpeed={100} cursorChar={' '}/> */}
+			</p>
+			<p>
+				<Typed strings={['The Digital World Is My Canvas', "Learning MERN Full-Stack Development", "Learning Tableau, Python and R", "Learning UIPath RPA"]} typeSpeed={50} backSpeed={30}  backDelay= {2000} cursorChar={'\u2588'}   loop/>
+			</p>
 		</div>
 
 		<Nav></Nav>

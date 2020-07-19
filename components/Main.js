42c70dbfbb387d4c01fecc7a03823299ac13e379
frinzekt <frinzekt@gmail.com>
2020-07-19 23:56:36 +0800

    remove media sizing

--- components/Main.js
+++ components/Main.js
@@ -7,24 +7,19 @@ export const Main = () => (
 		<a className='anchor' id='main'></a>
 		<div className='title'>Some Statistics About Me</div>
 		<div className='container' style={{ textAlign: 'center' }}>
-			<p className='style1' style={{ marginBottom: 10 }}>
-				<strong>More To Be Added Soon</strong>
-			</p>
 			{/* <a href='#' className='image featured'>
 				<img src='images/pic01.jpg' alt='' />
 			</a> */}
 
 			{/* <Features></Features> */}
 			<a className='anchor' id='statistics'></a>
-			<div className='row'>
-				<div className='col-6 col-12-small'>
-					<h3>Coding Activity For The Last 30 Days</h3>
-					<WakatimeBar></WakatimeBar>
-				</div>
-				<div className='col-6 col-12-small'>
-					<h3>Languages Used For The Last 30 Days</h3>
-					<WakatimePie></WakatimePie>
-				</div>
+			<div className='container'>
+				<h3>Coding Activity For The Last 30 Days</h3>
+				<WakatimeBar></WakatimeBar>
+			</div>
+			<div className='container'>
+				<h3>Languages Used For The Last 30 Days</h3>
+				<WakatimePie></WakatimePie>
 			</div>
 		</div>
 	</section>
